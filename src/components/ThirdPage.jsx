1b0371388e32a025add7524886e7f8465363a1e4
karma2912 <rajakyash23@gmail.com>
2024-06-11 00:31:14 +0530

    Third Done

--- src/components/ThirdPage.jsx
+++ src/components/ThirdPage.jsx
@@ -21,20 +21,20 @@ const ThirdPage = () => {
             </div>
             <div className='service'>
             <i class="fa-solid fa-user"></i>
-                <p className='inner-guidance'>Expert Guidance</p><br/>
-                <p className='inner-guidance-para'>A beacon of knowledge lighting the way through life's maze, offering clarity
+                <p className='inner-guidance'>Personalised Services</p><br/>
+                <p className='inner-guidance-para'>Tailored experiences, unique solutions, individual attention, customized offerings, specialized assistance, one-on-one support.
                 </p>
             </div>
             <div className='process'>
             <i class="fa-solid fa-file"></i>
-                <p className='inner-guidance'>Expert Guidance</p><br/>
-                <p className='inner-guidance-para'>A beacon of knowledge lighting the way through life's maze, offering clarity
+                <p className='inner-guidance'>Transparent Process</p><br/>
+                <p className='inner-guidance-para'>Tailored experiences with clear communication, transparent procedures, personalized recommendations, open process.
                 </p>
             </div>
             <div className='support'>
             <i class="fa-solid fa-handshake"></i>
-                <p className='inner-guidance'>Expert Guidance</p><br/>
-                <p className='inner-guidance-para'>A beacon of knowledge lighting the way through life's maze, offering clarity
+                <p className='inner-guidance'>Exceptional Support</p><br/>
+                <p className='inner-guidance-para'>Tailored experiences with transparent procedures, customized solutions, and personalized recommendations
                 </p>
             </div>
         </div>
